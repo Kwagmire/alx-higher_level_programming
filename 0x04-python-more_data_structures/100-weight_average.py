64b1b8e6787c0ea515bfc4247b3d70773c4ad6f1
Kwagmire <kwagmire999@gmail.com>
2022-07-07 05:25:17 +0100

    Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,6 +6,6 @@ def weight_average(my_list=[]):
     den = 0
 
     for tup_le in my_list:
-        num += tup[0] * tup[1]
+        num += (tup[0] * tup[1])
         den += tup[1]
     return num / den
