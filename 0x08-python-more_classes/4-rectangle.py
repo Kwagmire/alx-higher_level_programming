82e288f3d1cefee865268164b95c3235253a6da5
Kwagmire <kwagmire999@gmail.com>
2022-07-26 08:32:49 +0100

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -75,4 +75,4 @@ class Rectangle:
     def __repr__(self):
         """Return the string representation of the Rectangle"""
         ret = "Rectangle (" + str(self.__width)
-        return ret + ", " + str(self.__height) + ")"
+        return ret += ", " + str(self.__height) + ")"
