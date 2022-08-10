558983f92ca91a6971dd749121c8b50860c1aba3
Kwagmire <kwagmire999@gmail.com>
2022-08-10 20:51:34 +0100

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Task 2 of ALX Project(Python - Inheritance)
+
+This module defines a function that confirms an object-class relationship.
+
+"""
+
+
+def is_same_class(obj, a_class):
+    """Check if the specified object is exactly an instance of the specified class.
+
+    Args:
+        obj: the object
+        a_class: the class
+
+    Returns:
+        bool: True, if there's a direct relationship, and False, if otherwise.
+    
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
