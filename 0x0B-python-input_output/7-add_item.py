ddd781b7b5e0637242515348d1d201b95361c80d
Kwagmire <kwagmire999@gmail.com>
2022-08-11 14:11:44 +0100

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,12 @@ This module uses its arguments to modify a JSON file.
 """
 import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-theList = load_from_json_file("add_item.json")
-
-if type(theList) != list:
+try:
+    theList = load_from_json_file("add_item.json")
+except FileNotFoundError:
     theList = []
 
 for string in sys.argv[1:]:
