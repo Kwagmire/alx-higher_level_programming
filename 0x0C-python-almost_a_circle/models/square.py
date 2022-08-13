97e7c6c6f66eb4ca3e14c2e4b91b90095e649270
Kwagmire <kwagmire999@gmail.com>
2022-08-13 07:43:34 +0100

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Task 10 of ALX Project(Python - Almost a circle)
+
+This module defines a `Square` class.
+
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a Square.
+
+        Args:
+            size (int): The length of the square
+            x (int): (Optional) Its x-coordinate
+            y (int): (Optional) Its y-coordinate
+            id (int): (Optional) Its identity
+
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.width)
